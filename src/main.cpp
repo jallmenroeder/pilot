92891138f644d784f789813e91e4536063914869
jallmenroeder <contact@jallmenroeder.de>
2021-10-20 17:41:09 +0200

    add external libraries to cmake

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <glm/glm.hpp>
+
+int main() {
+    glm::vec3 vec(1, 2, 3);
+
+    std::cout << "Hello, World! Vec: " << vec.x << " " << vec.y << " " << vec.z << std::endl;
+    return 0;
+}
