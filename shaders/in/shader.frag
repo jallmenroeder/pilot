5869ce8ac1c20d7d773ef8ee0f5bda1c75a05de0
jallmenroeder <contact@jallmenroeder.de>
2023-08-31 11:47:20 +0200

    add: normal and uv in shaders

--- shaders/in/shader.frag
+++ shaders/in/shader.frag
@@ -1,11 +1,13 @@
 
 #version 450
 
-layout(location = 0) in vec3 fragColor;
+layout(location = 0) in vec3 inNormal;
+layout(location = 1) in vec2 inTexCoord;
 
 layout(location = 0) out vec4 outColor;
 
 void main() {
-    outColor = vec4(fragColor, 1.0);
+    outColor = vec4(inNormal, 1.0);
+//    outColor = vec4(inTexCoord, 1.0, 1.0);
 //    outColor = vec4(1., 0., 0., 1.0);
 }
--- src/PilotEngine.cpp
+++ src/PilotEngine.cpp
@@ -35,8 +35,9 @@ const std::vector<const char*> deviceExtensions = {
 };
 
 struct Vertex {
-    glm::vec3 pos;
-    glm::vec3 color;
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 texCoord;
 
     static VkVertexInputBindingDescription getBindingDescription() {
         VkVertexInputBindingDescription bindingDescription{};
@@ -47,30 +48,34 @@ struct Vertex {
         return bindingDescription;
     }
 
-    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
-        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+    static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions() {
+        std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions{};
         attributeDescriptions[0].binding = 0;
         attributeDescriptions[0].location = 0;
         attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attributeDescriptions[0].offset = offsetof(Vertex, pos);
+        attributeDescriptions[0].offset = offsetof(Vertex, position);
         attributeDescriptions[1].binding = 0;
         attributeDescriptions[1].location = 1;
         attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attributeDescriptions[1].offset = offsetof(Vertex, color);
+        attributeDescriptions[1].offset = offsetof(Vertex, normal);
+        attributeDescriptions[2].binding = 0;
+        attributeDescriptions[2].location = 2;
+        attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[2].offset = offsetof(Vertex, texCoord);
         return attributeDescriptions;
     }
 };
 
-// unit cube
+// unit cube (no sensible data in normals and uv, just for testing)
 const std::vector<Vertex> VERTICES = {
-        {{-0.5f, -0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-        {{0.5f, -0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
-        {{0.5f, 0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}},
-        {{-0.5f, 0.5f, -0.5f}, {1.0f, 1.0f, 1.0f}},
-        {{-0.5f, -0.5f, 0.5f}, {1.0f, 0.0f, 0.0f}},
-        {{0.5f, -0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-        {{0.5f, 0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
-        {{-0.5f, 0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}}
+        {{-0.5f, -0.5f, -0.5f}, glm::normalize(glm::vec3(1.0f, 0.0f, 0.0f)), {0., 0.}},
+        {{0.5f, -0.5f, -0.5f}, glm::normalize(glm::vec3(0.0f, 1.0f, 0.0f)), {1., 0.}},
+        {{0.5f, 0.5f, -0.5f}, glm::normalize(glm::vec3(0.0f, 0.0f, 1.0f)), {1., 1.}},
+        {{-0.5f, 0.5f, -0.5f}, glm::normalize(glm::vec3(1.0f, 1.0f, 1.0f)), {0., 1.}},
+        {{-0.5f, -0.5f, 0.5f}, glm::normalize(glm::vec3(1.0f, 0.0f, 0.0f)), {1., 1.}},
+        {{0.5f, -0.5f, 0.5f}, glm::normalize(glm::vec3(0.0f, 1.0f, 0.0f)), {0., 1.}},
+        {{0.5f, 0.5f, 0.5f}, glm::normalize(glm::vec3(0.0f, 0.0f, 1.0f)), {0., 0.}},
+        {{-0.5f, 0.5f, 0.5f}, glm::normalize(glm::vec3(1.0f, 1.0f, 1.0f)), {1., 1.}}
 };
 
 const std::vector<uint16_t> INDICES = {
